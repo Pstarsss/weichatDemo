e30df4699324b41e3326b4d053ef65b92d8628e4
DESKTOP-QFGIF0N\YQSL <1377689310@qq.com>
2020-12-06 10:18:12 +0800

    wqe

--- pages/user/index.js
+++ pages/user/index.js
@@ -1,20 +1,49 @@
 // pages/user/index.js
+let music;
+import { request } from '../../request/index.js'
+import regeneratorRuntime  from '../../lib/runtime/runtime.js'
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    wx.login({
+      success:function(res){
+        console.log(res);
+      },
+      fail:function(err){
+        console.log(err);
+      }
+    })
   },
+  
 
+  /**
+   * 获取歌曲的数据
+   */
+    async getMusicList(){
+        // const list = await wx.request({
+        //   url: 'http://tingapi.ting.baidu.com/v1/restserver/ting',
+        //   method: 'get',
+        //   data:{
+        //     method:'baidu.ting.billboard.billList',
+        //     type:2,
+        //     size:10,
+        //     offset:0
+        //   },
+        //   success(res){
+        //     console.log((res));
+        //   }
+        // })
+    },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
